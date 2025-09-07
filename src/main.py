894cd43d9749e08a76be5c3acbd2819ce6176986
Takashi Kitao <takashi.kitao@gmail.com>
2025-09-07 16:03:48 +0900

    Added metadata and a pyxapp file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+# title: Megaball
+# author: Adam
+# desc: A short arcade shooter inspired by Defender
+# site: https://github.com/helpcomputer/laser-jetman
+# license: MIT
+# version: 1.0
+
 from app import App
 
 if __name__ == "__main__":
